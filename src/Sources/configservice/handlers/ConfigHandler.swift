8a933f05c039053157d432ea74d1520b5e8c77a5
Kiera O'Reilly <kforeilly7@hotmail.com>
2016-07-22 08:59:57 +0100

    Send back correct config

--- src/Sources/configservice/handlers/ConfigHandler.swift
+++ src/Sources/configservice/handlers/ConfigHandler.swift
@@ -10,7 +10,7 @@ public class ConfigHandler {
   public static func handle(statsD: StatsDProtocol, abBranch: String = "a", config: JSON, complete: (status: HTTPStatusCode, data: JSON?) -> Void) -> Void {
     statsD.timer(bucket: "\(Buckets.Application.rawValue).\(Buckets.HealthHandler.rawValue).\(Buckets.Get.rawValue).\(Buckets.Timing.rawValue)") {
       let configForUser = config["ios"][abBranch]
-      complete(status: HTTPStatusCode.OK, data: getConfigForUser(abBranch, configForUser))
+      complete(status: HTTPStatusCode.OK, data: configForUser)
     }
   }
 }
