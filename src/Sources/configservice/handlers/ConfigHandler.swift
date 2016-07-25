4e6de340b68a4641552a5dcc46f4357c4c7375ec
Kiera O'Reilly <kforeilly7@hotmail.com>
2016-07-25 15:55:00 +0100

    Extract param validation to middleware

--- src/Sources/configservice/handlers/ConfigHandler.swift
+++ src/Sources/configservice/handlers/ConfigHandler.swift
@@ -7,34 +7,16 @@ import StatsD
 
 public class ConfigHandler {
 
-  public static func handle(statsD: StatsDProtocol, config: JSON, params: [String: String], complete: (status: HTTPStatusCode, data: JSON?) -> Void) -> Void {
+  public static func handle(statsD: StatsDProtocol, config: JSON, abBranch: String, complete: (status: HTTPStatusCode, data: JSON?) -> Void) -> Void {
     statsD.timer(bucket: "\(Buckets.Application.rawValue).\(Buckets.ConfigHandler.rawValue).\(Buckets.Get.rawValue).\(Buckets.Timing.rawValue)") {
-      let (validParams, abBranch) = validateParams(params: params)
-
-      if validParams && abBranch != nil {
-        let configForUser = config["ios"][abBranch!]
-        complete(status: HTTPStatusCode.OK, data: configForUser)
-      } else {
-        Log.error("Invalid request")
-        statsD.increment(bucket: "\(Buckets.Application.rawValue).\(Buckets.ConfigHandler.rawValue).\(Buckets.Post.rawValue).\(Buckets.Called.rawValue).\(Buckets.BadRequest.rawValue)")
-        complete(status: HTTPStatusCode.badRequest, data: nil)
-      }
+        if config["ios"][abBranch] != nil {
+          statsD.increment(bucket: "\(Buckets.Application.rawValue).\(Buckets.ConfigHandler.rawValue).\(Buckets.Get.rawValue).\(Buckets.Called.rawValue).\(Buckets.Success.rawValue).Branch.\(abBranch)")
+          complete(status: HTTPStatusCode.OK, data: config["ios"][abBranch])
+        } else {
+          Log.error("Invalid request")
+          statsD.increment(bucket: "\(Buckets.Application.rawValue).\(Buckets.ConfigHandler.rawValue).\(Buckets.Get.rawValue).\(Buckets.Called.rawValue).\(Buckets.BadRequest.rawValue)")
+          complete(status: HTTPStatusCode.badRequest, data: nil)
+        }
     }
-  }
-
-  private static func validateParams(params: [String: String]?) -> (valid: Bool, id: String?) {
-    if params != nil, let id = params!["abBranch"] {
-      return (paramValid(param: id), id)
-    }
-
-    return (true, nil)
-  }
-
-  private static func paramValid(param: String) -> Bool {
-    if param.characters.count == 1 {
-      return true
-    }
-
-    return false
   }
 }
---
+++ src/Sources/configservice/middleware/ValidationMiddleware.swift
@@ -0,0 +1,47 @@
+import Foundation
+import StatsD
+import Kitura
+import KituraNet
+import LoggerAPI
+
+public class ValidationMiddleware: RouterMiddleware {
+
+  let statsD: StatsDProtocol
+
+  public init(statsD: StatsDProtocol) {
+    self.statsD = statsD
+  }
+
+  public func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
+    let (validParams, abBranch) = validateParams(params: request.parameters)
+
+    if validParams && abBranch != nil {
+      next()
+    } else {
+      do {
+        Log.error("Invalid request")
+        statsD.increment(bucket: "\(Buckets.Application.rawValue).\(Buckets.ConfigHandler.rawValue).\(Buckets.Get.rawValue).\(Buckets.Called.rawValue).\(Buckets.BadRequest.rawValue)")
+        response.status(HTTPStatusCode.badRequest)
+        try response.end()
+      } catch {
+        Log.error("Error")
+      }
+    }
+  }
+
+  private func validateParams(params: [String: String]?) -> (valid: Bool, branch: String?) {
+    if params != nil, let branch = params!["abBranch"] {
+      let valid =  paramValid(param: branch)
+      return (valid, branch)
+    }
+
+    return (false, nil)
+  }
+
+  private func paramValid(param: String) -> Bool {
+    if param.characters.count != 1 {
+      return false
+    }
+    return false
+  }
+}
