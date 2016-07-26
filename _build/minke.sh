b7e6ddfd88c1b618a2649f8926b258a13c5e04b6
Nic Jackson <jackson.nic@gmail.com>
2016-07-26 14:40:51 +0100

    Did some stuff that might work
    testing

--- _build/minke.sh
+++ _build/minke.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
-DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
-GEMSET='minkegems'
-RVM_COMMAND="source /usr/local/rvm/scripts/rvm && rvm gemset use ${GEMSET}"
+MINKE_VERSION="1.12.7"
 ERROR="Please specify a command e.g. ./minke.sh rake app:test"
-COMMAND=""
+
+DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
 NEW_UUID=$(base64 /dev/urandom | tr -d '/+' | head -c 32 | tr '[:upper:]' '[:lower:]')
+
+GEMSET=$(<.ruby-gemset)
+LEN=$(echo ${#GEMSET})
+
+if [ $LEN -lt 1 ]; then
+  GEMSET='minkegems'
+fi
+
 GEMSETFOLDER="/usr/local/rvm/gems/ruby-2.3.1@${GEMSET}"
+RVM_COMMAND="source /usr/local/rvm/scripts/rvm && rvm gemset use ${GEMSET} --create"
+DOCKER_IMAGE="nicholasjackson/minke:${MINKE_VERSION}"
+COMMAND=""
+
 
 if [ "$1" == '' ]; then
- echo $ERROR;
- exit 1;
+  echo $ERROR;
+  exit 1;
 fi
 
 COMMAND=$*
 
 if [[ $1 == \ -g* ]]; then
- echo "Generating new template"
- DIR=${PWD}
- DOCKER_RUN="docker run --rm -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR} nicholasjackson/minke /bin/bash -c '${RVM_COMMAND} && bundle install && minke ${COMMAND}'"
- eval "${DOCKER_RUN}"
+  echo "Generating new template"
+  DIR=${PWD}
+  DOCKER_RUN="docker run --rm -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR} ${DOCKER_IMAGE} /bin/bash -c '${RVM_COMMAND} && bundle install && minke ${COMMAND}'"
+  eval "${DOCKER_RUN}"
 fi
 
 if [[ $1 != \ -g* ]]; then
- DIR=$(dirname `pwd`)
- DOCKER_RUN="docker run --rm --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build nicholasjackson/minke /bin/bash -c '${RVM_COMMAND} && ${COMMAND}'"
- #echo $DOCKER_RUN
- echo "Running command: ${COMMAND}"
+  DIR=$(dirname `pwd`)
+  DOCKER_RUN="docker run --rm --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build ${DOCKER_IMAGE} /bin/bash -c '${RVM_COMMAND} && ${COMMAND}'"
 
- eval "docker network create minke_${NEW_UUID}"
- eval "${DOCKER_RUN}"
- eval "docker network rm minke_${NEW_UUID}"
+  echo "Running command: ${COMMAND}"
+
+  eval "docker network create minke_${NEW_UUID}"
+  eval "${DOCKER_RUN}"
+  eval "docker network rm minke_${NEW_UUID}"
 fi
