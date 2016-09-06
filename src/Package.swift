8edf150885250b88b15188dd46745066c834eb0d
tonygreen <tonygreen@notonthehighstreet.com>
2016-09-06 15:29:52 +0100

    updated package version for swift 3.6

--- src/Package.swift
+++ src/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         name: "configservice")
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 20),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 0, minor: 9),
-      .Package(url: "https://github.com/notonthehighstreet/swift-statsd", majorVersion: 0, minor: 4)
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 28),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 0, minor: 15),
+      .Package(url: "https://github.com/notonthehighstreet/swift-statsd", majorVersion: 0, minor: 5)
     ])
