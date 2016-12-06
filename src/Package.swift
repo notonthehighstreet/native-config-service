b1ba1f078dbb8b433e7970b3708f1473fda19a51
Nic Jackson <jackson.nic@gmail.com>
2016-12-06 11:25:12 +0000

    Updated package versions

--- src/Package.swift
+++ src/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         name: "configservice")
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
       .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 3),
       .Package(url: "https://github.com/notonthehighstreet/swift-statsd", majorVersion: 0, minor: 7)
     ]
